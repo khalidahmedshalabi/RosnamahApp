7b1fc357c3eaafdce8abf15f1d7f949e675d661e
Khalid <khalidahmed333@hotmail.com>
2018-06-18 19:47:55 +0200

    Remove template reducer from app reducers

--- redux/index.js
+++ redux/index.js
@@ -2,14 +2,12 @@ import { combineReducers } from 'redux';
 
 // Import all reducers
 import { reducer as LangRedux } from './LangRedux';
-import { reducer as TemplateRedux } from './TemplateRedux';
 import { reducer as FirstRunStuffRedux } from './FirstRunStuffRedux';
 import { localeReducer as locale } from 'react-localize-redux';
 
 // Combine the imported reducers
 const AppReducers = combineReducers({
 	language: LangRedux,
-	template: TemplateRedux,
 	firstRun: FirstRunStuffRedux,
 	locale
 });
