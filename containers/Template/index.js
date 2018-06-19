1ab9048b9bddbbf25032532eeee92cd6132ad664
Khalid <khalidahmed333@hotmail.com>
2018-06-19 17:44:32 +0200

    Add example on how to translate app text

--- containers/Template/index.js
+++ containers/Template/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { getTranslate } from 'react-localize-redux';
 
 export default class Template extends Component {
 	render () {
+		const { translate } = this.props
+
 		return (
 			<View>
+				{/*		translate() gets the value of "Hello" that is in constants/Languages.js	*/}
+				<Text>{translate('Hello')}</Text>
+				<Text>Khalid</Text>
 			</View>
 		)
 	}
 }
+
+const mapStateToProps = (state) => ({
+	translate: getTranslate(state.locale),
+})
+
+export default connect(mapStateToProps)(Template)
