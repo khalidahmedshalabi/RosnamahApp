9e835219d66abb80a778d37cb79eb2902e39ce01
Khalid <khalidahmed333@hotmail.com>
2018-06-20 15:48:04 +0200

    Add home screen - #7

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,10 +1,11 @@
 export const mainColor = '#7568aa';
 export const secondColor = '#503b70';
+export const bgColor = '#f7f7f7';
 
 const colors = {
 	mainColor,
 	secondColor,
-	bgGray: '#eeeeee',
+	bgColor,
 }
 
 export default colors
--- containers/Home/index.js
+++ containers/Home/index.js
@@ -1,18 +1,79 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { FlatList, View, TouchableOpacity, Image, Text } from 'react-native'
 import { connect } from 'react-redux'
 import { getTranslate } from 'react-localize-redux';
+import { Container } from 'native-base';
+import { bgColor, secondColor } from '../../constants/Colors';
 
 class Home extends Component {
+	state = {
+		categories: [
+			{
+				key: '1',
+				name: 'Saudi Arabia',
+				img: 'http://saudiexpatriate.com/wp-content/uploads/2016/11/Kingdom-Centre-Tower-Riyadh-Top-Shopping-Malls-in-Saudi-Arabia-SaudiExpatriate.com_.png'
+			},
+			{
+				key: '2',
+				name: 'Russia',
+				img: 'https://vacationidea.com/pix/img25Hy8R/articles/best-russia-destinations_f.jpg'
+			},
+			{
+				key: '3',
+				name: 'United States',
+				img: 'https://techround.co.uk/wp-content/uploads/2018/04/bridge-2.jpg'
+			},
+			{
+				key: '4',
+				name: 'Spain',
+				img: 'https://arzotravels.com/wp-content/uploads/2017/11/valencia-Best-places-to-go-in-Spain-1000x666.jpg'
+			},
+			{
+				key: '5',
+				name: 'England',
+				img: 'http://feelindia.org/wp-content/uploads/2015/11/in-england-best-places-tourist-spots-to-visit-in-england-uk.jpg'
+			},
+			{
+				key: '6',
+				name: 'Italy',
+				img: 'http://travelsandliving.com/wp-content/uploads/2015/02/best-places-to-visit-in-Italy-Manarola-Liguria-e1423069720386.jpg'
+			},
+		]
+	}
+
+	renderCategory = (item, index) => {
+		return (
+			<TouchableOpacity
+				style={{ flex: 1, height: 250, borderRadius: 2, marginHorizontal: 5, backgroundColor: 'white' }}>
+				<Image
+					resizeMode='cover'
+					style={{ flex: 0.8 }}
+					source={{ uri: item.img }} 
+					/>
+				
+				<View style={{ flex: 0.2, paddingHorizontal: 10, justifyContent: 'center' }}>
+					<Text style={{ color: secondColor, fontSize: 17 }}>{item.name}</Text>
+				</View>
+			</TouchableOpacity>
+		)
+	} 
+
 	render () {
 		const { translate } = this.props
 
 		return (
-			<View>
-				{/*		translate() gets the value of "Hello" that is in constants/Languages.js	*/}
-				<Text>{translate('Hello')}</Text>
-				<Text>Khalid</Text>
-			</View>
+			<Container style={{ backgroundColor: bgColor }}>
+				<FlatList
+					contentContainerStyle={{ paddingVertical: 10 }}
+					numColumns={2}
+					data={this.state.categories}
+					style={{ flex: 1 }}
+					ItemSeparatorComponent={
+						() => <View style={{ height: 10, backgroundColor: 'transparent' }}></View>
+					}
+					renderItem={({ item, index }) => this.renderCategory(item, index)}
+				/>
+			</Container>
 		)
 	}
 }
