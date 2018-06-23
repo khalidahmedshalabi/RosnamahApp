df14c253ecbb11820aee8ab3be433d4c260f09f9
Khalid <khalidahmed333@hotmail.com>
2018-06-23 22:41:56 +0200

    Add Settings to drawer and change "Save" button behavior

--- navigation/MainDrawerNavigator.js
+++ navigation/MainDrawerNavigator.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { I18nManager } from 'react-native'
-import { DrawerNavigator, createStackNavigator } from 'react-navigation';
+import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
 import Octicons from 'react-native-vector-icons/Octicons'
 import { mainColor } from '../constants/Colors'
 import CustomDrawerContentComponent from './CustomDrawerContent';
 
 import Home from '../containers/Home'
+import Settings from '../containers/Settings'
 
-const MainDrawerNavigator = DrawerNavigator({
+const drawerIconSize = 26
+
+const MainDrawerNavigator = createDrawerNavigator({
 
 	Home: {
-		screen: Home, navigationOptions: {
+		screen: Home, 
+		navigationOptions: {
 			drawerIcon: ({ tintColor }) => (
-				<Octicons name='home' color={mainColor} size={26} tintColor={tintColor} />
+				<Octicons name='home' color={mainColor} size={drawerIconSize} tintColor={tintColor} />
+			),
+		}
+	},
+	Settings: {
+		screen: Settings, 
+		navigationOptions: {
+			drawerIcon: ({ tintColor }) => (
+				<Octicons name='gear' color={mainColor} size={drawerIconSize} tintColor={tintColor} />
 			),
 		}
 	},
