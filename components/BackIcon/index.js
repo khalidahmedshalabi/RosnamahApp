4fd9e38198671a59ba311d7fe21b79ab02635386
Khalid <khalidahmed333@hotmail.com>
2018-07-06 01:08:11 +0200

    Add default style for BackIcon component

--- components/BackIcon/index.js
+++ components/BackIcon/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Platform, I18nManager } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import styles from './styles'
+import { mainColor } from '../../constants/Colors';
 
 export default BackIcon = ({ color, style, size }) => {
 	// Gives the correct icon based on current language settings (RTL or LTR)
@@ -13,8 +14,8 @@ export default BackIcon = ({ color, style, size }) => {
     return (
 		<Ionicons
 			name={this.determineAppropriateIcon()}
-			size={size}
-			color={color}
+			size={size ? size : 25}
+			color={color ? color : mainColor}
 			style={[styles.icon, style]} />
 	)
 }
